cf5ca74c2ff6283d0a5598dde65bc9daf446b3c1
codinaven <anshunit2023@gmail.com>
2026-08-28 02:08:51 +0530

    Fix some bugs in auth

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,24 +4,22 @@ import { userLogin, userRegister } from "../services/auth.service.js";
 export const userRegistered = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const result = await userRegister(req.body);
-    console.log(result)
     if (!result) {
-      res.status(400).json({ message: "user registration failed", data: result })
-
+      return res.status(400).json({ message: "user registration failed", data: result })
     }
-    res.status(200).json({ message: "user register succesfully", data: result })
+    return res.status(200).json({ message: "user register succesfully", data: result })
   } catch (error) {
     next(error);
   }
 }
 
-export const signInUser = async (req: Request, res: Response) => {
+export const signInUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const result = await userLogin(req.body);
     console.log(result);
     res.cookie("token", result);
-    res.status(200).json({ message: "Login succesfully" })
+    return res.status(200).json({ message: "Login succesfully" })
   } catch (error) {
-    res.status(404).json({ message: "token not get", error: error })
+    next(error);
   }
 } 
--- src/utils/token.ts
+++ src/utils/token.ts
@@ -3,16 +3,15 @@ export interface JwtPayload {
   id: number;
 }
 
-export const generateToken = (Id: number) => {
-  try {
-    if (Id == null) {
-      throw new Error("Id not found");
-    }
-    const token = jwt.sign({ data: Id }, process.env.JWT_SECRET!, { expiresIn: '1h' })
-    return token;
-  } catch (error) {
-    console.log(error);
+export const generateToken = (id: number): string => {
+  if (id == null) {
+    throw new Error("Id not found");
   }
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined");
+  }
+  const token = jwt.sign({ id }, process.env.JWT_SECRET!, { expiresIn: '1h' })
+  return token;
 }
 
 export const validateToken = (token: string): JwtPayload => {
@@ -22,8 +21,12 @@ export const validateToken = (token: string): JwtPayload => {
     if (typeof result === 'string') {
       throw new Error("Invalid Token Payload");
     }
-
-    return result as JwtPayload;
+    if (typeof result.id !== "number") {
+      throw new Error("Invalid Token Payload");
+    }
+    return {
+      id: result.id
+    };
   } catch (error) {
     console.log(error);
     throw new Error("Invalid Token");
